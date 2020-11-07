d0db5c40bfd0c8376794582436fb6da570e3e391
DPK <dougkvamme@gmail.com>
2020-11-07 12:45:02 -0600

    setting up local storage, set & get functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-logic
+// variables for the celebrity name fields  //
+let celebSearch = document.querySelector("#celebSearch");
+let celebNameButton = document.querySelector("#celebName");
+
+
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute("class", type);
+}
+
+// event listener for the celeb name button //
+
+celebNameButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+// create celeb object from submission //
+  let celeb = {
+    celebSearch: celebSearchInput.value.trim(),
+  };
+
+  console.log(celeb);
+  
+ // validate the field to make sure its not empty //
+ if (celeb.celebSearch === "") {
+  displayMessage("error", "Field cannot be blank");
+} else {
+  displayMessage("success", "Bingo!");
+
+  // set new celeb submission //
+  localStorage.setItem("celeb", JSON.stringify(celeb));
+    
+  // get most recent celeb submission //
+  let lastCeleb = JSON.parse(localStorage.getItem("celeb"));
+  
+  }
+});
