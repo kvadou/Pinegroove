6b1b0c7af705f90f7305c534abe1b9dec2132d72
Troy Dorman <troydorman@gmail.com>
2020-11-07 09:20:15 -0600

    got basic search query working for giphy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-logic
+const apiKey = "&api_key=ZZMVzE78mVVCOYcbnGuHdsZrKPcFpH0A";
+const gifURL = "http://api.giphy.com/v1/gifs/search?";
+const stickerURL = "http://api.giphy.com/v1/stickers/search";
+
+
+$.ajax({
+    url: gifURL + "q=cheeseburgers" + apiKey,
+    method: "GET"
+
+
+}).then(function(response){
+
+    console.log(response);
+
+
+
+
+
+});
